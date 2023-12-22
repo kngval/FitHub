162e3e2b04f1eee409e9924bc218284db83b8a6c
kngval <brentvalino61@gmail.com>
2023-12-22 15:04:12 +0800

    small adjustments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,13 +25,13 @@ const loginUser = async(req,res) => {
 //signup
 
 const signupUser = async(req,res) => {
-    const { email,password } = req.body
+    const { email,password } = req.body 
     
     try {
         const user = await UserModel.signup(email,password);
         
         const token = createToken(user._id)
-
+    
         res.status(200).json({email, token})      
     } catch (error) {
          res.status(400).json({error: error.message})
