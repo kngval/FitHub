b4497254838a53e9fef58169d5776871bded7601
kngval <bkvalteocs@gmail.com>
2023-12-22 18:16:26 +0800

    creating unaccessible if not logged in

--- frontend/src/components/CreateWorkout.jsx
+++ frontend/src/components/CreateWorkout.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import WorkoutModal from './WorkoutModal';
+import { useAuthContext } from '../../hooks/useAuthContext';
+function CreateWorkout() {
 
-function CreateWorkout({onWorkoutAdded}) {
-    
     const [modalToggle,setModalToggle] = useState(false);
+    const [error, setError] = useState(null);
+    const {user} = useAuthContext();
 
+  const handleModal =()=>{
+    
+    if(!user){
+      setError('You must Login first')
+      return
+    }
+    openModal()
+  }
 
     const openModal = () => {
         setModalToggle(true)
@@ -16,8 +26,11 @@ function CreateWorkout({onWorkoutAdded}) {
 
   return (
     <div className='text-center mt-5'>
-      <button  className='border-2 rounded-3xl border-black p-2' onClick={openModal}>Create A New Workout</button>
-      <WorkoutModal  isOpen={modalToggle} onClose={closeModal}  onWorkoutAdded={onWorkoutAdded}/>
+      <button  className='border-2 rounded-3xl border-black p-2' onClick={handleModal}>Create A New Workout</button>
+      <WorkoutModal  isOpen={modalToggle} onClose={closeModal}/>
+      {error && (<div className="w-full border-red bg-red-100 text-lg p-4 rounded-md mt-3">
+        {error}
+      </div>)}
     </div>
   );
 }
