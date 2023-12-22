43fc6bc7adbbb33f23b83d247f3012bddf2e7213
kngval <bkvalteocs@gmail.com>
2023-12-22 18:56:23 +0800

    cannot access home if not logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 //PAGES
 import Home from "../pages/Home";
 import { WorkoutContextProvider } from "./context/WorkoutContext";
@@ -7,25 +8,32 @@ import Signup from "../pages/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
-
-
+  const { user } = useAuthContext();
 
   return (
     <WorkoutContextProvider>
       <div className="App">
         <BrowserRouter>
-        <Navbar />
+          <Navbar />
           <div className="pages">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-
+              <Route
+                path="/"
+                element={user ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
             </Routes>
           </div>
         </BrowserRouter>
       </div>
-      </WorkoutContextProvider>
+    </WorkoutContextProvider>
   );
 }
 
