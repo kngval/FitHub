69cdf6b9046163bb11764a5aeca15d3a5bd67764
kngval <bkvalteocs@gmail.com>
2023-12-23 21:20:58 +0800

    added logo and ui changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,18 +15,18 @@ const Navbar = () => {
       <div className=" shadow-md p-6 flex justify-between items-center">
         <Link to="/">
         <div className="flex items-center">
-          <img src={WorkoutLogo} alt=""  className="w-10 mr-3" />
-          <h1>FitHub</h1>
+          <img src={WorkoutLogo} alt=""  className="w-7 sm:w-10 mr-3" />
+          <h1 className="text-xs sm:text-sm">FitHub</h1>
         </div>
         </Link>
 
         <nav>
           {user && (
             <div>
-              <span className="mr-2">{user.email}</span>
+              <span className="text-xs sm:text-sm mr-2 ">{user.email}</span>
               <button
                 onClick={handleClick}
-                className="bg-blue-500 text-white p-1.5 1111 mr-4 "
+                className="logout rounded-md text-white p-1.5 1111 mr-4 text-xs sm:text-sm"
               >
                 Logout
               </button>
@@ -35,10 +35,10 @@ const Navbar = () => {
           {!user && (
 
           <div>
-            <Link className="bg-blue-500 text-white p-2 mr-4 " to="/login">
+            <Link className="loginBtn text-white p-2 mr-4 rounded-md" to="/login">
               Login
             </Link>
-            <Link className="bg-blue-500 text-white p-2 mr-4 " to="/signup">
+            <Link className="signupBtn text-white p-2 mr-4 rounded-md" to="/signup">
               Signup
             </Link>
           </div>
